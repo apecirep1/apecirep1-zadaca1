19aef412a8b21de0cd818e5ffdf1a369d7b364ab
Andrej <apecirep1@etf.unsa.ba>
2022-11-19 18:01:43 +0100

    added function check

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -8,22 +8,50 @@ public class ExpressionEvaluator {
     private static Stack<String> ops = new Stack<> ();
     private static Stack<Double> vals = new Stack<> ();
 
-    public static String[] parsedString(String str){
+    public static String[] parsedStr(String str){
 
         return str.split(" ");
     }
 
-    public static double val(String op, double x1, double x2){
+    public static void check(String string){
+
+        String [] parstr = parsedStr(string);
+        for(String s: parstr){
+
+            if(!(s.equals("(") || s.equals(")") || s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("sqrt")
+                    || s.equals("0") || s.equals("1") || s.equals("2") || s.equals("3") || s.equals("4")
+                    || s.equals("5") || s.equals("6") || s.equals("7") || s.equals("8") || s.equals("9")))
+                throw new RuntimeException("Error: Wrong operands or operators");
+        }
+
+        int leftpar = 0;
+        int rightpar = 0;
+        for(String s: parstr) {
+            if (s.equals("(")){
+                leftpar = leftpar + 1;
+            }
+
+
+            if (s.equals(")")){
+                rightpar = rightpar + 1;
+            }
+        }
+
+        if (rightpar != leftpar)  throw new RuntimeException("Error: Invalid parentheses");
+
+    }
+
+    public static double val(String operator, double x1, double x2){
         double res = 0;
-        if(op.equals("+")){
+        if(operator.equals("+")){
             res=x1+x2;
-        } else if(op.equals("-")){
+        } else if(operator.equals("-")){
             res=x1-x2;
-        } else if(op.equals("*") ){
+        } else if(operator.equals("*") ){
             res=x1*x2;
-        } else if(op.equals("/")){
+        } else if(operator.equals("/")){
             res=x1/x2;
-        }else if(op.equals("sqrt")) {
+        }else if(operator.equals("sqrt")) {
             res=Math.sqrt(x2);
         }
 
