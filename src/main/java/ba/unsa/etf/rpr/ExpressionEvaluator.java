dac9d1e05cd1b02bfe415bf5cade6733412daea5
Andrej <apecirep1@etf.unsa.ba>
2022-11-19 17:09:04 +0100

    added function that returns the result depending on the operator

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -1,4 +1,34 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Stack;
+
 public class ExpressionEvaluator {
+
+
+    private static Stack<String> ops = new Stack<> ();
+    private static Stack<Double> vals = new Stack<> ();
+
+    public static String[] parsedString(String str){
+
+        return str.split(" ");
+    }
+
+    public static double val(String op, double x1, double x2){
+        double res = 0;
+        if(op.equals("+")){
+            res=x1+x2;
+        } else if(op.equals("-")){
+            res=x1-x2;
+        } else if(op.equals("*") ){
+            res=x1*x2;
+        } else if(op.equals("/")){
+            res=x1/x2;
+        }else if(op.equals("sqrt")) {
+            res=Math.sqrt(x2);
+        }
+
+        return res;
+    }
+
+
 }
