cc295d47c4621aae2543bee2846d0eb3cfb4fef1
Andrej <apecirep1@etf.unsa.ba>
2022-11-19 16:46:10 +0100

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main( String[] args ){
+        String str = "";
+        for(int i = 0; i < args.length; i++)str+=args[i];
+
+        str += " ";
+
+
+    }
+}
